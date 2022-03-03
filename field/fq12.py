a2ce7f018c145ab6b7c995319cdb330b84cb29ef
Jinank Jain <jinank94@gmail.com>
2022-03-03 18:05:30 +0530

    field, testing: Add support for cyclomatic squaring for Fq12
    Signed-off-by: Jinank Jain <jinank94@gmail.com>

--- field/fq12.py
+++ field/fq12.py
@@ -109,3 +109,69 @@ class Fq12:
             self.val[0].frobenius_map(power),
             self.val[1].frobenius_map(power).mul_by_fq2(self.frobenius_coeffs_c1[power % 12]),
         ])
+
+    def square(self):
+        ab = self.val[0] * self.val[1]
+
+        term1 = ((self.val[0] + self.val[1]) * (self.val[0] + self.mul_by_residue(self.val[1]))) - (ab + self.mul_by_residue(ab))
+        term2 = ab + ab
+
+        return self.__class__([term1, term2])
+
+    def cyclotomic_square(self):
+        z0 = self.val[0].val[0]
+        z1 = self.val[0].val[1]
+        z2 = self.val[0].val[2]
+        z3 = self.val[1].val[0]
+        z4 = self.val[1].val[1]
+        z5 = self.val[1].val[2]
+
+        tmp = z0 * z1
+        t0 = (z0 + z1) * (z0 + Fq6.non_residue * z1) - tmp - Fq6.non_residue * tmp
+        t1 = tmp + tmp
+        # t2 + t3*y = (z2 + z3*y)^2 = b^2
+        tmp = z2 * z3
+        t2 = (z2 + z3) * (z2 + Fq6.non_residue * z3) - tmp - Fq6.non_residue * tmp
+        t3 = tmp + tmp
+        # t4 + t5*y = (z4 + z5*y)^2 = c^2
+        tmp = z4 * z5
+        t4 = (z4 + z5) * (z4 + Fq6.non_residue * z5) - tmp - Fq6.non_residue * tmp
+        t5 = tmp + tmp
+
+        # for A
+
+        # z0 = 3 * t0 - 2 * z0
+        z0 = t0 - z0
+        z0 = z0 + z0
+        z0 = z0 + t0
+        # z1 = 3 * t1 + 2 * z1
+        z1 = t1 + z1
+        z1 = z1 + z1
+        z1 = z1 + t1
+
+        # for B
+
+        # z2 = 3 * (xi * t5) + 2 * z2
+        tmp = Fq6.non_residue * t5
+        z2 = tmp + z2
+        z2 = z2 + z2
+        z2 = z2 + tmp
+
+        # z3 = 3 * t4 - 2 * z3
+        z3 = t4 - z3
+        z3 = z3 + z3
+        z3 = z3 + t4
+
+        # for C
+
+        # z4 = 3 * t2 - 2 * z4
+        z4 = t2 - z4
+        z4 = z4 + z4
+        z4 = z4 + t2
+
+        # z5 = 3 * t3 + 2 * z5
+        z5 = t3 + z5
+        z5 = z5 + z5
+        z5 = z5 + t3
+
+        return self.__class__([Fq6([z0, z4, z3]), Fq6([z2, z1, z5])])
